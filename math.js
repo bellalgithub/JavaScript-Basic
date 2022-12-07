6501f7d8dd37031f006b3c4fe3703efa42de0582
Bellal <bellalsq@gmail.com>
2022-12-08 02:54:39 +0600

    updated

---
+++ math.js
@@ -0,0 +1,57 @@
+
+let price1 = -200.5;
+
+let price2 = Math.abs(price1); // It converts negative to positive number
+console.log(price2);
+
+// Output: 200.5
+
+let price3 = -200.4999;
+
+let price4 = Math.round(price3); // It converts the number into round
+console.log(price4);
+
+// Output:  -200 
+
+let price5 = -200.500001;
+
+let price6 = Math.round(price5); // It converts the number into round
+console.log(price6);
+
+// Output:  -201
+
+let price7 = 200.500001;
+
+let price8 = Math.ceil(price7); // It converts the number into ceil number
+console.log(price8);
+
+// Output:  201 
+
+let price9 = 200.500001;
+
+let price10 = Math.floor(price9); // It converts the number into floor number
+console.log(price10);
+
+// Output:  200
+
+// Random Number
+
+let number = Math.random();
+console.log(number);
+
+// outpt : 0.86666666 ;
+
+let number1 = Math.random() * 10;
+console.log(Math.round(number1));
+
+// Output: 9, 10, 2, 3, 5, 6, 7, 8
+
+
+
+
+
+
+
+
+
+
