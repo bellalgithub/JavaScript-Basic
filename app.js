166ed7b1d5d075769af011865b19b49e2196faca
Bellal <bellalsq@gmail.com>
2022-12-08 00:26:03 +0600

    Js Basic

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let name = 'A quick Brown Fox Jumps Over the lazy dog ';
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.indexOf('Jumps'));
+console.log(name.indexOf('jumps'));
+
+
